ae7ad4da323227a836ed8150f504c97a39a562f4
Daniel Ragsdale <DanJeffRags@gmail.com>
2024-03-12 13:04:42 -0600

    Added more photos to the image carousel.

--- pages/index.js
+++ pages/index.js
@@ -8,18 +8,39 @@ export default function Home() {
 	<>
 		<div className="carousel">
 			<Image
-				src="/rainier.jpg"
-				width={902}
+				src="/carousel/rainier.jpg"
+				width={438}
 				height={600}
 				quality={90}
 				alt="Mt. Rainier"
 			/>
 			<Image
-				src="/rappel.jpg"
+				src="/carousel/moab_traverse.jpg"
+				width={800}
+				height={600}
+				quality={90}
+				alt="Moab Utah"
+			/>
+			<Image
+				src="/carousel/rappel.jpg"
 				width={356}
 				height={600}
 				quality={90}
 				alt="Rappelling in Custer State Park"
+			/>
+			<Image
+				src="/carousel/25_short.jpg"
+				width={929}
+				height={600}
+				quality={90}
+				alt="Ski touring in Grand Teton National Park"
+			/>
+			<Image
+				src="/carousel/monkey_face.jpg"
+				width={623}
+				height={600}
+				quality={90}
+				alt="Climbing Monkey Face at Smith Rock State Park"
 			/>
 		</div>
 		<Header title="Welcome to danragsdale.com!" />
