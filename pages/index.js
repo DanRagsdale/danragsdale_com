bd92f6d3200b0d223b0f77c100b73d522f7383a1
Daniel Ragsdale <DanJeffRags@gmail.com>
2024-03-19 12:29:51 -0600

    Created basic content for the homepage.

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ import styles from '/components/index.module.css';
 export default function Home() {
 	// TODO dynamically populate this from a larger pool of images
 	// TODO dynamically adjust viewable length to account for screen size
+	// TODO calculate needed offset by position, not as an accumulator
 	const [images, setImages] = useState([
 		'monkey_face.jpg', // Copy of Final
 		'rainier.jpg',
@@ -81,15 +82,41 @@ export default function Home() {
 			</div>
 			<div className={styles.overlay}></div>
 			<div className={styles.booking}>
-					<h1>Let's have an adventure!</h1>
+					<h1>Ready for Adventure!</h1>
 					<Link href="/schedule" className={styles.bookingButton}>Schedule Now!</Link>
 			</div>
 		</div>
+
 		<div className={styles.textBody}>
-			<Header title="Welcome to danragsdale.com!" />
-			<p className="description">
-				<Link href="/schedule">Check out this other page!</Link>
-			</p>
+			<em>The following is placeholder text, and does not reflect my actual services or certifications.</em>
+			<h1>Climbing, Mountaineering, and Skiing</h1>
+			<div>
+				<p>
+					Epic climbs, powder days, breathtaking views - that's the life we love, and I'm here to share it with you!
+					I'm Daniel Ragsdale, a certified ski instructor and single pitch instructor based in Jackson Hole, Wyoming.
+					Whether you're a first-timer yearning for fresh mountain air or a seasoned mountaineer seeking new challenges, I'm your guide to unforgettable mountain experiences.	
+				</p>
+				<h3>Services Offered:</h3>
+				<ul>
+					<li>
+						<strong>Ski Instruction:</strong> Conquer the slopes of Jackson Hole.
+						I tailor lessons to all levels, helping you refine your technique or discover the joy of skiing for the first time.
+					</li>
+					<li>
+						<strong>Climbing Guiding:</strong> Scale new heights safely with expert guidance.
+						I offer a variety of climbing experiences in Jackson Hole and beyond, from introductory top-roping to exhilarating multi-day expeditions.
+						Together, let's reach the summit of your climbing goals.
+					</li>
+					<li>
+						<strong>Mountaineering:</strong> Embark on an unforgettable journey through breathtaking landscapes.
+						My mountaineering services unlock the true wilderness experience, with expert planning and safety protocols to ensure a successful and thrilling adventure.
+					</li>
+				</ul>
+				Ready to test your strength, conquer a peak, or explore the backcountry?
+			</div>
+			<div>
+				<Link href="/schedule" className={styles.bottomBookingButton}>Let's plan your mountain adventure!</Link>
+			</div>
 		</div>
 	</>
 	)
