62962ea2e04bd9340960ce3627f8c56a14fdddb2
Daniel Ragsdale <DanJeffRags@gmail.com>
2024-03-19 15:54:57 -0600

    Added logo to navbar.

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,16 +1,27 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 import styles from './NavBar.module.css'
 
 export default function Footer() {
-  return (
+	return (
 	<div className={styles.nav_bar}>
 		<ul>
-			<li><Link href="/climb" className={styles.nav_link}>Climb</Link></li>
-			<li><Link href="/ski" className={styles.nav_link}>Ski</Link></li>
-			<li><Link href="/code" className={styles.nav_link}>Code</Link></li>
-			<li style={{float:"right"}}><Link href="/second_page" className={styles.nav_link}>Blog</Link></li>
+			<li>
+				<Link href="/">
+					<Image
+						src="/logo.png"
+						width={110}
+						height={80}
+						alt="DanRagsdale.com Logo"/>
+				</Link>
+			</li>
+			<li><Link href="/climb"><span className={styles.center}>Climb</span></Link></li>
+			<li><Link href="/ski"><span className={styles.center}>Ski</span></Link></li>
+			
+			<li style={{float:"right"}}><Link href="/second_page"><span className={styles.center}>Blog</span></Link></li>
+			<li style={{float:"right"}}><Link href="/code"><span className={styles.center}>Code</span></Link></li>
 		</ul>
 	</div>
-  )
+	)
 }
