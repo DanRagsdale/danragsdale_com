2fed04634bc8cfca1a56c9e3ac6f722fac8650b2
Daniel Ragsdale <DanJeffRags@gmail.com>
2024-03-20 13:51:34 -0600

    Created a personalized footer with my socials.

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,14 @@
+import Link from 'next/link'
+
 import styles from './Footer.module.css'
 
 export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+				<Link href="https://venmo.com/u/DanRagsdale"><img src="/logo-venmo.svg" alt="Venmo Logo" className={styles.logo} style={{marginRight:"0px"}}/></Link>
+				<Link href="https://instagram.com/DanRagsdale"><img src="/logo-instagram.svg" alt="Instagram Logo" className={styles.logo} /></Link>
+				<Link href="https://github.com/DanRagsdale"><img src="/logo-github.svg" alt="Github Logo" className={styles.logo} /></Link>
       </footer>
     </>
   )
