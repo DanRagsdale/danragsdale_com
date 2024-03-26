5eb79b1cbd1ff48af859fc196d7510cad47e52c7
Daniel Ragsdale <DanJeffRags@gmail.com>
2024-03-26 11:53:33 -0600

    Added links to code projects on the code page.

--- pages/code/index.js
+++ pages/code/index.js
@@ -1,15 +1,22 @@
-import styles from '@styles/code_index.module.css'; 
+import styles from '@styles/code/index.module.css'; 
+
+import CodeBlock from '@components/CodeBlock'
 
 export default function Home() {
   return (
 	<>
-		<div className={styles.textBody}>
+		<div className='textBody'>
 			<h1>Coding projects</h1>
 			<p>
 				When I am not exploring the mountains, I love to mess around with software development.
 				Below are a collection of some of my interesting projects.
 				Checkout my GitHub for more!
 			</p>
+		</div>
+		<div className='textBody' style={{maxWidth: "initial", width : "90%"}}>
+			<div className={styles.gridContainer}>
+				<CodeBlock href="/code/pace" text="Pace Calculator" image="/running.jpg"/>
+			</div>
 		</div>
 	</>
   )
