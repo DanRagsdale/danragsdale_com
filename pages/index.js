ad802faa25dc73d0bd025dfbd59c33aac2fd000b
Daniel Ragsdale <DanJeffRags@gmail.com>
2024-06-12 19:15:57 -0600

    Updated copy to reflect current certifications.

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
 		<Carousel 
 			images={[
 				'rainier.jpg',
+				'franz.jpg',
 				'moab_traverse.jpg',
 				'rappel.jpg',
 				'25_short.jpg',
@@ -22,7 +23,6 @@ export default function Home() {
 		</Carousel>
 
 		<div className={styles.textBody}>
-			<em>The following is placeholder text, and does not reflect my actual services or certifications.</em>
 			<h1>Climbing, Skiing, Mountaineering</h1>
 			<div>
 				<p>
@@ -30,23 +30,27 @@ export default function Home() {
 					I'm Daniel Ragsdale, a certified ski instructor and single pitch instructor based in Jackson Hole, Wyoming.
 					Whether you're a first-timer yearning for fresh mountain air or a seasoned mountaineer seeking new challenges, I'm your guide to unforgettable mountain experiences.	
 				</p>
-				<h3>Services Offered:</h3>
+				<h2>Trips Offered:</h2>
 				<ul>
 					<li>
-						<strong>Climbing Guiding:</strong> Scale new heights safely with expert guidance.
-						I offer a variety of climbing experiences in Jackson Hole and beyond, from introductory top-roping to exhilarating multi-day expeditions.
-						Together, let's reach the summit of your climbing goals.
+						<h3 className={styles.tripOption}>Via Ferrata</h3> 
+						Join me on an exhilarating via ferrata journey through the rugged beauty of Jackson Hole! 
+						As your personal guide, I'll lead you along steep faces, bridges, and cliffside paths, ensuring your safety while maximizing the adventure. 
+						Whether you're a climbing enthusiast or a novice eager to explore, this customized trip promises an unforgettable blend of adrenaline and awe-inspiring views.
 					</li>
 					<li>
-						<strong>Ski Instruction:</strong> Conquer the slopes of Jackson Hole.
-						I tailor lessons to all levels, helping you refine your technique or discover the joy of skiing for the first time.
+						<h3 className={styles.tripOption}>Top Rope Climbing</h3>
+						Top rope climbing is the perfect way to learn new skills and test your strength in a safe environment.
+						As your instructor, I will select routes to meet your goals and find the perfect blend of challenge and reward.
+						Enjoy the thrill of climbing and the beauty of the outdoors with the security of a top rope and a certified single pitch instructor.
+
 					</li>
 					<li>
-						<strong>Mountaineering:</strong> Embark on an unforgettable journey through breathtaking landscapes.
-						My mountaineering services unlock the true wilderness experience, with expert planning and safety protocols to ensure a successful and thrilling adventure.
+						<h3 className={styles.tripOption}>Ski Instruction</h3> 
+						I offer ski lessons in Jackson Hole for all ability levels.
 					</li>
 				</ul>
-				Ready to test your strength, conquer a peak, or explore the backcountry?
+				Ready for the ultimate adventure?
 			</div>
 			<div>
 				<Link href="/schedule" className={styles.bottomBookingButton}>Let's plan your mountain adventure!</Link>
